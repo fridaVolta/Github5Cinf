84ba82203ab070a3c2c1f51e8c9297a64f02a283
frida <frida@DESKTOP-L72T31C>
2024-10-15 08:57:46 +0200

    primo commit

---
+++ src/main/java/com/mycompany/github5cinf/Github5Cinf.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.github5cinf;
+
+/**
+ *
+ * @author frida
+ */
+public class Github5Cinf {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        
+    }
+}
